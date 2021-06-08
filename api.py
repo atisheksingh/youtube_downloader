029ebc1b80a44b08341e1730165ad36dff91899f
atisheksingh <84221898+atisheksingh@users.noreply.github.com>
2021-06-08 12:14:59 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,55 @@
-import requests #api-module
-
+from pytube import YouTube
+from tkinter.filedialog import *
+from tkinter.messagebox import *
+from tkinter import *
+from threading import *
+import requests
 import json
+def completeDownload(stream=None, file_path=None):
+    print("download completed")
+    showinfo("Message", "File has been downloaded...")
+    downloadBtn['text'] = "Download Video"
+    downloadBtn['state'] = "active"
+    urlField.delete(0, END)
+# onprogress callbackfunction
+def progressDownload(stream=None, chunk=None, bytes_remaining=None):
+    percent = (100 * ((file_size - bytes_remaining) / file_size))
+    downloadBtn['text'] = "{:00.0f}% downloaded ".format(percent)
+# download function
+def startDownload(url):
+    global file_size
+    path_to_save = askdirectory()
+    if path_to_save is None:
+        return
+
+    try:
+        yt = YouTube(url)
+        st = yt.streams.first()
+
+        yt.register_on_complete_callback(completeDownload)
+        yt.register_on_progress_callback(progressDownload)
+
+        file_size = st.filesize
+        st.download (output_path=path_to_save, filename=str(b[1]))
+
+    except Exception as e:
+        print(e)
+        print("something went wrong")
+
+#button funtion
+def btnClicked():
+    try:
+        downloadBtn['text'] = "Please wait..."
+        downloadBtn['state'] = 'disabled'
+        url = str(c[0])
+        if url == '':
+            return
+        print(url)
+        thread = Thread(target=startDownload, args=(url,))
+        thread.start()
+
+    except Exception as e:
+        print(e)
 
 def genrate_link():
     url = "http://smartgsc.rannlabprojects.com/api/CMS/SearchAdvertisement"
@@ -58,15 +107,11 @@ T.insert(END, Fact)
 
 l2=Label(text='url')
 l2.pack(side=TOP)
-T1 = Text(root, height = 1, width = 50)
-T1.pack(side=TOP)
-T1.insert(END, fact2)
+urlField = Text(root, height = 1, width = 50)
+urlField.pack(side=TOP)
+urlField.insert(END, fact2)
 
-downloadBtn = Button(root, text="Download Video", font=font, relief='ridge')#, command=btnClicked)
+downloadBtn = Button(root, text="Download Video", font=font, relief='ridge', command=btnClicked)
 downloadBtn.pack(side=TOP, pady=20)
 
-
-
-
 root.mainloop()
-
